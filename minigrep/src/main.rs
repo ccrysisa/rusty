9ebd1813617d389ad214820ab6902c8ed31c67b2
ccrysisa <gshine@m.scnu.edu.cn>
2024-05-24 21:01:59 +0800

    Add a cli program minigrep
    Building a Command Line Program as an I/O project.

---
+++ minigrep/src/main.rs
@@ -0,0 +1,22 @@
+use minigrep::run;
+use minigrep::Config;
+use std::env;
+use std::process;
+
+fn main() {
+    /* Accepting Command Line Arguments */
+    let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Probelm parsing argumrnts: {}", err);
+        process::exit(1);
+    });
+    // println!("Search for {}", config.query);
+    // println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
