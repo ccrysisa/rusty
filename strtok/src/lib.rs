13b74cb34ed4d4b0af9696f8d8c4c933b6e79d0b
ccrysisa <gshine@m.scnu.edu.cn>
2024-06-26 21:08:03 +0800

    Implement more powerful strtok

--- strtok/src/lib.rs
+++ strtok/src/lib.rs
@@ -11,6 +11,50 @@ pub fn strtok<'a>(s: &'_ mut &'a str, delimiter: char) -> &'a str {
     }
 }
 
+#[derive(Debug)]
+struct Delimiter<'a> {
+    index: usize,
+    delimter: &'a str,
+}
+
+pub fn strtokn<'a>(s: &'_ mut &'a str, delimters: &'_ [&'_ str]) -> &'a str {
+    let (i, l) = loop {
+        let mut dmt: Option<Delimiter> = None;
+        for &d in delimters {
+            if let Some(i) = s.find(d) {
+                dmt = match dmt {
+                    Some(dmt) => Some(Delimiter {
+                        index: std::cmp::min(dmt.index, i),
+                        delimter: if i < dmt.index { d } else { dmt.delimter },
+                    }),
+                    _ => Some(Delimiter {
+                        index: i,
+                        delimter: d,
+                    }),
+                };
+            }
+        }
+
+        // println!("{:?}", dmt);
+        match dmt {
+            Some(d) => {
+                if d.index == 0 {
+                    *s = &s[d.delimter.len()..];
+                    continue;
+                } else {
+                    break (d.index, d.delimter.len());
+                }
+            }
+            _ => break (s.len(), 0),
+        }
+    };
+
+    let prefix = &s[..i];
+    let suffix = &s[(i + l)..];
+    *s = suffix;
+    prefix
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,5 +68,31 @@ mod tests {
         let hello = strtok(&mut x, ' ');
         assert_eq!(hello, "Hello");
         assert_eq!(x, "world");
+    }
+
+    #[test]
+    fn test_strtokn() {
+        let mut x = "- This, a sample string.";
+        let delimters = [" ", ",", ".", "-"];
+
+        let mut tok = strtokn(&mut x, &delimters);
+        assert_eq!(tok, "This");
+        assert_eq!(x, " a sample string.");
+
+        tok = strtokn(&mut x, &delimters);
+        assert_eq!(tok, "a");
+        assert_eq!(x, "sample string.");
+
+        tok = strtokn(&mut x, &delimters);
+        assert_eq!(tok, "sample");
+        assert_eq!(x, "string.");
+
+        tok = strtokn(&mut x, &delimters);
+        assert_eq!(tok, "string");
+        assert_eq!(x, "");
+
+        tok = strtokn(&mut x, &delimters);
+        assert_eq!(tok, "");
+        assert_eq!(x, "");
     }
 }
